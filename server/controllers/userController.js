d0842a8df7e58b3086e097dc7fc80bc1d7eba8b1
vaniairnanda <vaniairnanda@gmail.com>
2020-01-25 20:29:00 +0700

    addproduct to cart

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,6 +40,67 @@ module.exports =  {
             product: req.body.product,
             quantity: 1,
         }
+        console.log(req.body.product, 'ini yg di input')
+        User.findOne({ _id: req.currentUserId})
+            .then(user => {
+                console.log(user, 'ini user')
+                let existing = false
+                if (user.cart.length === 0) {
+                    console.log('masuk yang length 0')
+                    User.updateOne(
+                        { _id: req.currentUserId },
+                        { $push: { cart: input } }
+                    )
+                    .then((user) =>{
+                       console.log(user);
+                       res.status(200).json(user)
+                    })
+                    .catch(err =>{
+                        next(err)
+                    })
+                } else {
+                    user.cart.forEach( (item, i, arr) => {
+                        console.log(item.product, 'ini yg di loop')
+                        console.log(req.body.product, 'ini yg dikirim')
+                        if (item.product == req.body.product) {
+                            existing = true
+                            let newQty = item.quantity += 1
+                            User.updateOne({
+                              'cart.product': item.product
+                            }, {
+                                '$set': {
+                                    'cart.$.quantity': newQty,
+                                }
+                            })
+                            .then((data) =>{
+                                console.log(data);
+                                res.status(200).json(data)
+                             })
+                             .catch(err =>{
+                                 next(err)
+                             })
+                            
+                        }
+                    })
+                    if (existing === false) {
+                        console.log('masuk bkn existing')
+                        User.updateOne(
+                            { _id: req.currentUserId },
+                            { $push: { cart: input } }
+                        )
+                        .then((user) =>{
+                           console.log(user);
+                           res.status(200).json(user)
+                        })
+                        .catch(err =>{
+                            next(err)
+                        })
+                    }
+                }
+            })
+            .catch(err =>{
+                next(err)
+            })
 
     }
 
