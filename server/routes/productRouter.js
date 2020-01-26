eab255557117971bda00b16294e1f6d4feb141e5
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-01-27 02:49:41 +0700

    dokumentasi (server) done

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -3,12 +3,16 @@ const productRouter = express.Router()
 const productController = require('../controllers/productController')
 const authentication = require('../middlewares/authentication')
 const authorization = require('../middlewares/authorization')
+const multer = require('multer')
 
-productRouter.post('/', authentication, authorization, productController.addProduct)
+// productRouter.post('/', authentication, authorization, productController.addProduct)
 productRouter.get('/', productController.getProduct)
 productRouter.get('/:id', productController.getById)
 productRouter.patch('/:id', authentication, productController.updateProductStock)
 productRouter.put('/:id', authentication, authorization, productController.updateProduct)
 productRouter.delete('/:id', authentication, authorization, productController.deleteProduct)
+productRouter
+  .route('/')
+  .post(multer({ dest: 'temp/', limits: { fieldSize: 3 * 1024 * 1024 } }).single('imageUrl'), authentication, authorization, productController.addProduct)
 
 module.exports = productRouter
