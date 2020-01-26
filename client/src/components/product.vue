0b4c6baef394973681562015b3eb50c6a1c524fe
nafies1 <ghuroba.muslim@gmail.com>
2020-01-27 05:51:09 +0700

    loop product. Details product on progress

--- client/src/components/product.vue
+++ client/src/components/product.vue
@@ -1,26 +1,41 @@
 <template>
   <div>
-    <b-card
-      title="Card Title"
-      img-src="https://s0.bukalapak.com/img/00257755741/w-1000/Sendok_Makan_Stainless_Murah___12_pcs___A.jpg.webp"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
-      </b-card-text>
-
-      <b-button href="#" variant="primary">Add to Cart</b-button>
-    </b-card>
+    <!-- {{products}} -->
+    <b-container fluid="sm">
+    <b-row class="justify-content-center">
+      <b-col 
+        sm="4"
+        v-for="product in products"
+        :key="product._id"
+        ><b-card
+          :title="product.name"
+          :img-src="product.image"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>
+            {{product.description}}
+          </b-card-text>
+          <b-button href="#" variant="info" class="mx-2">View details</b-button> 
+          <b-button href="#" variant="primary">Add to Cart</b-button>
+        </b-card>
+      </b-col >
+    </b-row>
+  </b-container>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'product',
+  computed: {
+    products () {
+      return this.$store.state.products
+    }
+  }
 }
 </script>
 
