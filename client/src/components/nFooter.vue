ebcb63b3689b3d1bd0109b328c6dea0bf26deb5c
nafies1 <ghuroba.muslim@gmail.com>
2020-01-27 18:32:07 +0700

    Fix product detail. Form add product for admin on progress

--- client/src/components/nFooter.vue
+++ client/src/components/nFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer-copyright text-center py-3 bg-primary mt-3">
     <div fluid style="color: white;">
-      &copy; 2020 Copyright: <a href="https://github.com/nafies1" style="color: white;"> Nav-shop </a>
+      &copy; 2020 Copyright: <a href="https://github.com/nafies1" target="_blank" style="color: white;"> Nav-shop </a>
     </div>
   </div>
 </template>
