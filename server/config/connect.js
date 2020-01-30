9691834c740ed0ecff74eb5f9ef0bebfa55c6f69
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-01-30 09:57:18 +0700

    benerin admin dikit

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect = () => {
-  mongoose.connect('mongodb://localhost:27017/ECommerce' + `-${process.env.NODE_ENV}`, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+  mongoose.connect('mongodb+srv://ninoloid:ahmadmsa@cluster0-4kxtr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, err => {
     if (err) console.log(err)
     else console.log('connected to database')
   })
